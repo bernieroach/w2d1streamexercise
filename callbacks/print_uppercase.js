19b808a021cd47a70812daeb52348e8cf2da7365
Bernard Roach <bernieroach@gmail.com>
2017-10-09 16:51:22 +0000

    callbacks upper lower reverse 1337

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,28 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+
+
+
+function toUpperCase(httpsString){
+  console.log(httpsString.toUpperCase());
+};
+
+function toLowerCase(httpsString){
+  console.log(httpsString.toLowerCase());
+};
+
+function reverse(httpsString){
+  console.log(httpsString.split("").reverse().join(""));
+};
+
+function 1337(httpsString){
+  console.log("not implemented yet");
+}
+
+
+getHTML(requestOptions,reverse);
